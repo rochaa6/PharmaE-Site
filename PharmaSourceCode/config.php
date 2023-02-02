cba0a2daf36d45d876ced2ccfc5b4b808b48ab7e
rochaa6 <104039970+rochaa6@users.noreply.github.com>
2023-02-01 22:11:21 -0500

    Add files via upload

--- PharmaSourceCode/config.php
+++ PharmaSourceCode/config.php
@@ -1,1 +1,7 @@
-
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'anthonyrochadatabase');
+   define('DB_PASSWORD', 'DoZ{@*_&V!X4');
+   define('DB_DATABASE', 'anthonyrochadatabase');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
